7ffa2dbba947cad9c469252c21f6690d1fb02e63
1tmc <158778648+1tmc@users.noreply.github.com>
2025-12-12 08:01:31 +0000

    Use Sanity hosted studio at h-vision.sanity.studio

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,15 @@
 /**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
+ * This configuration file lets you run `$ sanity [command]` in this folder
+ * Go to https://www.sanity.io/docs/cli to learn more.
+ **/
 import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig({ 
+  api: { projectId, dataset },
+  deployment: {
+    appId: 'lq7xb360x3ld1eq3rga5z0aa',
+  },
+})
